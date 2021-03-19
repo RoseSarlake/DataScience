bd4e8fce9ff01d3552198f0ba14c10e8267b8b14
Rose Sarlake <momorose@foxmail.com>
2021-03-19 16:46:07 +0000

    exe2 edit

--- root-DataScience/exercise/exercise2.r
+++ root-DataScience/exercise/exercise2.r
@@ -6,3 +6,11 @@ lda.model<-lda(Species~.,data=iris,subset = train)
 summary(lda.model)
 lda.pred<-predict(lda.model,newdata=iris[-train,])
 table(lda.pred$class,iris[-train,]$Species)
+
+totalSamples<-nrow(iris)
+train<- sample(totalSamples,totalSamples/2,replace = FALSE)
+test <- -train
+
+lda.model<-lda(Species~.,data=iris, subset=train)
+prediction<-predict(lda.model,newdata=iris[test,])
+table(prediction$class,iris[test,]$Species)
