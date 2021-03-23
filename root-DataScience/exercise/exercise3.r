d2223edc06a324c88760b30182771d0d991007ad
Rose Sarlake <momorose@foxmail.com>
2021-03-23 09:39:49 +0000

    smote update

--- root-DataScience/exercise/exercise3.r
+++ root-DataScience/exercise/exercise3.r
@@ -1,9 +1,18 @@
-library(DMwR2)
+library(smotefamliy)
 library(ISLR)
-set.seed(2)
+#library(ROSE)
+#set.seed(2)
 train<-sample(nrow(Default),nrow(Default)/2,replace = FALSE)
-ratio<-length(which(Default[train,]$default=="No"))/length(which(Default[train,]$default=="Yes"))
-dosmote<-SMOTE(default~.-student,data=Default[train,],perc.over=ratio*100,perc.under=100)
-doros<-ROS(default~.-student,data=Default[train,])
-length(which(dosmote$default=='No'))
-length(which(dosmote$default=='Yes'))
+#ratio<-length(which(Default[train,]$default=="No"))/length(which(Default[train,]$default=="Yes"))
+#dosmote<-SMOTE(default~.-student,data=Default[train,],perc.over=ratio*100,perc.under=100)
+smoted2<-SMOTE(Default[train,c("balance","income")],Default[train,"default"])
+#length(which(dosmote$default=='No'))
+#length(which(dosmote$default=='Yes'))
+table(smoted2$data$class)
+
+train.set<-Default[train,]
+t<-table(train.set$default)
+diff<-t[1]-t[2]
+minority.class<-train.set[train.set$default=="Yes"]
+selected<-sample(nrow(train.set),replace = TRUE)
+oversample.set<-rbind(train.set,minority.class[selected,])
